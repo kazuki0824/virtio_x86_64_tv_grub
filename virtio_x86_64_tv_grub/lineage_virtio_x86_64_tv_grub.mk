8a7f640a7c34f235ad379e8e7935544de4eeda19
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-08-13 15:58:07 +0900

    Remove fingerprint pinning

--- virtio_x86_64_tv_grub/lineage_virtio_x86_64_tv_grub.mk
+++ virtio_x86_64_tv_grub/lineage_virtio_x86_64_tv_grub.mk
@@ -13,8 +13,3 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
     ro.soc.model=$(PRODUCT_DEVICE)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="virtio_x86_64_tv-eng 14 AP2A.240705.005 0 test-keys"
-
-BUILD_FINGERPRINT := \
-    virtio/virtio_x86_64_tv/virtio_x86_64_tv:14/AP2A.240705.005/0:eng/test-keys
